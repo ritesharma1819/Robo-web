dc05ac3235312c5af65495b33c532323aa59a205
ritesharma1819 <96657505+ritesharma1819@users.noreply.github.com>
2022-02-20 22:34:53 +0530

    converted funtion of app in to class of app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 // added a new file for creating search box and title of app and also it cantain all body in this file 
 
-import React from "react";
+import React,{ Component } from "react";
 import Cardlist from "./Cardlist";
 import { robots } from "./robots";
 import Searchbox from "./Searchbox";
 
-const App = ()=>{
-    return(
-        <div className="tc">
-            <h1>Robots</h1>
-            <Searchbox/>
-            <Cardlist robots={robots}/>
-        </div>
-      
-    );
+class App extends Component {
+    constructor(){
+        super()
+        this.state={
+            robots: robots,
+            Searchbox: ''
+        }
+    }
+    render(){
+        return(
+            <div className="tc">
+                <h1>Robots</h1>
+                <Searchbox/>
+                <Cardlist robots={this.state.robots}/>
+            </div>
+          
+        );
+        }
+   
 }
 
 export default App;
