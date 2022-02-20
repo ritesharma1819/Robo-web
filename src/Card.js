0a11011443eba63bcae99d79d1d8baed3d3f65c3
ritesharma1819 <96657505+ritesharma1819@users.noreply.github.com>
2022-02-20 20:10:47 +0530

    fix the error which in robots file

--- src/Card.js
+++ src/Card.js
@@ -3,12 +3,12 @@
 import React from "react";
 
 const Card=(props)=>{
-    const {id, name, email}= props;
+    const { id, name, email}= props;
     return(
         <div className="bg-light-green dib br3 pa3 ma2 grow tc">
             < img src={`https://robohash.org//${id}?200x200`} alt="robots"/>
-            <h2>name</h2>
-            <p>email</p>
+            <h2>{name}</h2>
+            <p>{email}</p>
         </div>
     );
 }
--- src/robots.js
+++ src/robots.js
@@ -1,104 +1,104 @@
 // here list of robot with there id , name and email it is fake id , name and email which is taken from jasonplaceholder.typicode.com
 
-
-export default robots = [
+export const robots = [
     {
-      "id": 1,
-      "name": "id labore ex et quam laborum",
-      "email": "Eliseo@gardner.biz"
+      id: 1,
+      name: "id labore",
+      email: "Eliseo@gardner.biz"
     },
     {
-      "id": 2,
-      "name": "quo vero reiciendis velit similique earum",
-      "email": "Jayne_Kuhic@sydney.com"
+      id: 2,
+      name: "quo vero",
+      email: "Jayne_Kuhic@sydney.com"
     },
     {
-      "id": 3,
-      "name": "odio adipisci rerum aut animi",
-      "email": "Nikita@garfield.biz"
+      id: 3,
+      name: "odio adipisci",
+      email: "Nikita@garfield.biz"
     },
     {
-      "id": 4,
-      "name": "alias odio sit",
-      "email": "Lew@alysha.tv"
+      id: 4,
+      name: "alias odio",
+      email: "Lew@alysha.tv"
     },
     {
-      "id": 5,
-      "name": "vero eaque aliquid doloribus et culpa",
-      "email": "Hayden@althea.biz"
+      id: 5,
+      name: "vero eaque",
+      email: "Hayden@althea.biz"
     },
     {
-      "id": 6,
-      "name": "et fugit eligendi deleniti quidem qui sint nihil autem",
-      "email": "Presley.Mueller@myrl.com"
+      id: 6,
+      name: "et fugit",
+      email: "Presley.Mueller@myrl.com"
     },
     {
-      "id": 7,
-      "name": "repellat consequatur praesentium vel minus molestias voluptatum",
-      "email": "Dallas@ole.me"
+      id: 7,
+      name: "repellat vel",
+      email: "Dallas@ole.me"
     },
     {
-      "id": 8,
-      "name": "et omnis dolorem",
-      "email": "Mallory_Kunze@marie.org"
+      id: 8,
+      name: "et omnis",
+      email: "Mallory_Kunze@marie.org"
     },
     {
-      "id": 9,
-      "name": "provident id voluptas",
-      "email": "Meghan_Littel@rene.us"
+      id: 9,
+      name: "provident id",
+      email: "Meghan_Littel@rene.us"
     },
     {
-      "id": 10,
-      "name": "eaque et deleniti atque tenetur ut quo ut",
-      "email": "Carmen_Keeling@caroline.name"
+      id: 10,
+      name: "eaque et",
+      email: "Carmen_Keeling@caroline.name"
     },
     {
-      "id": 11,
-      "name": "fugit labore quia mollitia quas deserunt nostrum sunt",
-      "email": "Veronica_Goodwin@timmothy.net"
+      id: 11,
+      name: "fugit labore",
+      email: "Veronica_Goodwin@timmothy.net"
     },
     {
-      "id": 12,
-      "name": "modi ut eos dolores illum nam dolor",
-      "email": "Oswald.Vandervort@leanne.org"
+      id: 12,
+      name: "modi ut",
+      email: "Oswald.Vandervort@leanne.org"
     },
     {
-      "id": 13,
-      "name": "aut inventore non pariatur sit vitae voluptatem sapiente",
-      "email": "Kariane@jadyn.tv",
+      id: 13,
+      name: "aut pariatur",
+      email: "Kariane@jadyn.tv",
     },
     {
-      "id": 14,
-      "name": "et officiis id praesentium hic aut ipsa dolorem repudiandae",
-      "email": "Nathan@solon.io"
+      id: 14,
+      name: "et officiis",
+      email: "Nathan@solon.io"
     },
     {
-      "id": 15,
-      "name": "debitis magnam hic odit aut ullam nostrum tenetur",
-      "email": "Maynard.Hodkiewicz@roberta.com"
+      id: 15,
+      name: "debitis magnam ",
+      email: "Maynard.Hodkiewicz@roberta.com"
     },
     {
-      "id": 16,
-      "name": "perferendis temporibus delectus optio ea eum ratione dolorum",
-      "email": "Christine@ayana.info"
+      id: 16,
+      name: "perferendis ",
+      email: "Christine@ayana.info"
     },
     {
-      "id": 17,
-      "name": "eos est animi quis",
-      "email": "Preston_Hudson@blaise.tv",
+      id: 17,
+      name: "eos est ",
+      email: "Preston_Hudson@blaise.tv",
     },
     {
-      "id": 18,
-      "name": "aut et tenetur ducimus illum aut nulla ab",
-      "email": "Vincenza_Klocko@albertha.name",
+      id: 18,
+      name: "aut tenetur",
+      email: "Vincenza_Klocko@albertha.name",
     },
     {
-      "id": 19,
-      "name": "sed impedit rerum quia et et inventore unde officiis",
-      "email": "Madelynn.Gorczany@darion.biz",
+      id: 19,
+      name: "sed impedit",
+      email: "Madelynn.Gorczany@darion.biz",
     },
     {
-      "id": 20,
-      "name": "molestias expedita iste aliquid voluptates",
-      "email": "Mariana_Orn@preston.org",
-    }]
+      id: 20,
+      name: "molestias iste",
+      email: "Mariana_Orn@preston.org",
+    }
+]
