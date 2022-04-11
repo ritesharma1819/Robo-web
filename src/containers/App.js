074e5bc30a9bb42d1cfb8b38a93b5345e99fc13c
ritesharma1819 <96657505+ritesharma1819@users.noreply.github.com>
2022-04-12 02:36:43 +0530

    converted all class component into functional component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,6 @@
 // added a new file for creating search box and title of app and also it cantain all body in this file 
 
-import React,{ Component } from "react";
+import React,{ useEffect, useState } from "react";
 import Cardlist from "../components/Cardlist";
 import Searchbox from "../components/Searchbox";
 import './App.css'
@@ -16,15 +16,24 @@ import Scroll from '../components/Scroll'
 //     }
 
 const App=()=>{
+
+    const [robots, setRobots]=useState([]);
+    const [searchfiled, setSearchfiled]=useState('');
+
+    useEffect(()=>{
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response=> response.json())
+        .then(users => {setRobots(users)});
+    })
+
      const onSearchChange=(event)=>{
-        this.setState({searchfiled: event.target.value })
+        setSearchfiled(event.target.value )
     
     }
     // render(){
-        const {robots, searchfiled}= this.state;
         const filteredRobots = robots.filter(robot=>{
         return robot.name.toLowerCase().includes(searchfiled.toLowerCase());
-        })
+        },[])
         if(!robots.length){
             return <h1>Loading</h1>
         }
