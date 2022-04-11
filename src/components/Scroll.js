8a25893d4a5d90c98709af6c2412b37938d3f26f
ritesharma1819 <96657505+ritesharma1819@users.noreply.github.com>
2022-04-12 02:09:43 +0530

    fix height in scroll component

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -3,7 +3,7 @@ import React from "react";
 
 const Scroll = (props)=>{
     return(
-        <div style={{ overflow: 'scroll', border: '5px solid black', height: '500px' }}>
+        <div style={{ overflow: 'scroll', border: '5px solid black' , height: '600px' }}>
             {props.children}
         </div>
 
